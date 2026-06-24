de5048505b57926ae3da4824efc8035683aed4c0
PrathameshYende <yendeprathmesh@gmail.com>
2026-06-24 22:55:16 +0530

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -3,6 +3,7 @@ from selenium.webdriver.edge.service import Service
 from webdriver_manager.microsoft import EdgeChromiumDriverManager
 import pytest
 
+
 @pytest.fixture
 def driver():
     driver = webdriver.Edge(
@@ -16,3 +17,18 @@ def driver():
     yield driver
 
     driver.quit()
+
+
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_makereport(item):
+
+    outcome = yield
+    report = outcome.get_result()
+
+    if report.when == "call" and report.failed:
+
+        driver = item.funcargs["driver"]
+
+        driver.save_screenshot(
+            f"screenshots/{item.name}.png"
+        )
